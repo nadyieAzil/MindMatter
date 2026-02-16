f71dca4652137827b72b503f11000515283f662e
nadyieAzil <nadiazil4211@gmail.com>
2026-02-16 19:55:14 +0800

    Add files via upload

---
+++ Mind Matters/Mind Matters/Mind_MattersApp.swift
@@ -0,0 +1,32 @@
+//
+//  Mind_MattersApp.swift
+//  Mind Matters
+//
+//  Created by STDC_3 on 16/02/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Mind_MattersApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Mind Matters/Mind MattersUITests/Mind_MattersUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Mind_MattersUITestsLaunchTests.swift
+//  Mind MattersUITests
+//
+//  Created by STDC_3 on 16/02/2026.
+//
+
+import XCTest
+
+final class Mind_MattersUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

4c15ef174ecc47c2f458fad9dffc102e37c5084d
nadyie <2025137437@student.uitm.edu.my>
2026-02-17 09:50:55 +0800

    Add meditation screen UI

--- Mind Matters/Mind Matters/ContentView.swift
+++ Mind Matters/Mind Matters/ContentView.swift
@@ -35,7 +35,7 @@ struct ContentView: View {
                 }
             }
         } detail: {
-            Text("Select an item")
+            Text("Sila pilih barang weyh")
         }
     }
 
